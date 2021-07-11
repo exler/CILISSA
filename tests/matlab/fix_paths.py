7da6cd7f2a0c489a4d38ae48bf06f01d010834f2
Kamil Marut <kamil.marut@pm.me>
2021-07-11 19:00:08 +0200

    tests: Replace multiple data.json with single for convenience

--- tests/matlab/fix_paths.py
+++ tests/matlab/fix_paths.py
@@ -3,24 +3,23 @@ import os
 from pathlib import Path
 
 if __name__ == "__main__":
-    path = Path(os.path.dirname(__file__), "..", "data")
-    print(f"Checking {path.absolute()}")
+    path = Path(os.path.dirname(__file__), "..", "data", "data.json")
+    print(f"Fixing data.json in {path.absolute()}")
 
-    for root, dirs, files in os.walk(path, topdown=False):
-        if "data.json" in files:
-            print(f"Fixing data.json in {path.absolute()}")
+    fp = open(path, "rb")
+    data = json.load(fp)
+    fp.close()
 
-            fp = open(Path(root, "data.json"), "rb")
-            data = json.load(fp)
-            fp.close()
+    for dir_row in data:
+        for val_row in dir_row.values():
+            for row in val_row:
+                for r in row:
+                    p = Path(r["reference"])
+                    r["reference"] = str(p.relative_to(*p.parts[:1]))
 
-            for row in data:
-                p = Path(row["reference"])
-                row["reference"] = str(p.relative_to(*p.parts[:1]))
+                    p = Path(r["measured"])
+                    r["measured"] = str(p.relative_to(*p.parts[:1]))
 
-                p = Path(row["measured"])
-                row["measured"] = str(p.relative_to(*p.parts[:1]))
-
-            fp = open(Path(root, "data.json"), "w")
-            json.dump(data, fp)
-            fp.close()
+    fp = open(path, "w")
+    json.dump(data, fp)
+    fp.close()
--- tests/matlab/metrics.m
+++ tests/matlab/metrics.m
@@ -1,13 +1,16 @@
+%#ok<*AGROW>
+
 close all; clear; clc;
 
 base_path = "../data";
 ref_base_path = "../data/ref_images";
 excluded = [".", "..", "ref_images"];
+data = {};
 
 files = dir(base_path);
 for i = 1:length(files)
     if files(i).isdir && ~any(strcmp(excluded, files(i).name))
-        data = {};
+        dir_data = {};
         dir_path = fullfile(base_path, files(i).name);
         images = dir(dir_path);
         
@@ -15,15 +18,17 @@ for i = 1:length(files)
                 if ~images(j).isdir
                     ref_path = fullfile(ref_base_path, ref_image(images(j).name));
                     meas_path = fullfile(dir_path, images(j).name);
-                    data = [data, {compare(ref_path, meas_path)}]; %#ok<AGROW>
+                    dir_data = [dir_data, {compare(ref_path, meas_path)}];
                 end
         end
         
-        data = jsonencode(data);
-        fid = fopen(fullfile(base_path, files(i).name, "data.json"), "w");
-        if fid == -1, error("Cannot create JSON file"); end
-        fwrite(fid, data, "char");
-        fclose(fid);
+        data = [data, {containers.Map([files(i).name], {dir_data})}]; 
     end
 end
+
+data = jsonencode(data);
+fid = fopen(fullfile(base_path, "data.json"), "w");
+if fid == -1, error("Cannot create JSON file"); end
+fwrite(fid, data, "char");
+fclose(fid);
 
