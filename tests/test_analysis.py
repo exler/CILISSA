925dbe691c564e54b57435ac01345ae70567ef64
Kamil Marut <kamil.marut@pm.me>
2021-07-11 20:10:44 +0200

    tests: Add tests for metrics quality

---
+++ tests/test_analysis.py
@@ -0,0 +1,63 @@
+import json
+import os
+import unittest
+from pathlib import Path
+
+from cilissa.analyzers import ImageAnalyzer
+from cilissa.images import Image, ImagePair
+from cilissa.metrics import MSE, PSNR, SSIM
+from numpy import isinf
+
+
+class TestImageAnalysis(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.base_path = os.path.dirname(__file__)
+
+        fp = open(Path(cls.base_path, "data", "data.json"))
+        cls.metrics_data = json.load(fp)
+
+    def test_metric_mse(self) -> None:
+        mse = MSE()
+
+        for m_type in self.metrics_data.values():
+            for m in next(iter(m_type)):
+                ref_image = Image(Path(self.base_path, m["reference"]))
+                mea_image = Image(Path(self.base_path, m["measured"]))
+                image_pair = ImagePair(ref_image, mea_image)
+
+                result = mse.analyze(image_pair)
+
+                self.assertAlmostEqual(result, m["metrics"]["mse"], delta=0.1)
+
+    def test_metric_psnr(self) -> None:
+        psnr = PSNR()
+
+        for m_type in self.metrics_data.values():
+            for m in next(iter(m_type)):
+                ref_image = Image(Path(self.base_path, m["reference"]))
+                mea_image = Image(Path(self.base_path, m["measured"]))
+                image_pair = ImagePair(ref_image, mea_image)
+
+                result = psnr.analyze(image_pair)
+
+                if m["metrics"]["psnr"] is None:
+                    self.assertTrue(isinf(result))
+                else:
+                    self.assertAlmostEqual(result, m["metrics"]["psnr"], delta=0.1)
+
+    def test_metric_ssim(self) -> None:
+        ssim = SSIM()
+
+        for m_type in self.metrics_data.values():
+            for m in next(iter(m_type)):
+                ref_image = Image(Path(self.base_path, m["reference"]))
+                mea_image = Image(Path(self.base_path, m["measured"]))
+                image_pair = ImagePair(ref_image, mea_image)
+
+                result = ssim.analyze(image_pair)
+
+                self.assertAlmostEqual(result, m["metrics"]["ssim"], delta=0.5)
+
+    def test_image_analyzer(self) -> None:
+        pass
--- tests/test_basic.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class TestBasic(unittest.TestCase):
-    def test_check(self):
-        self.assertEqual(1, 1)
