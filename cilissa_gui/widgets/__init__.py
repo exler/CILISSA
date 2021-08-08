76df2bcd857963c614118800e41695d5b739b9dd
Kamil Marut <kamil.marut@pm.me>
2021-08-08 15:38:14 +0200

    chore: Fix type checking

--- cilissa_gui/widgets/__init__.py
+++ cilissa_gui/widgets/__init__.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPalette
 
@@ -7,10 +9,12 @@ class BackgroundColorMixin:
     Mixin used to see the boundaries of QWidget. Useful while debugging.
     """
 
+    palette: QPalette
+
     def __init__(self) -> None:
         super().__init__()
 
         palette = self.palette()
         palette.setColor(QPalette.Window, Qt.red)
-        self.setAutoFillBackground(True)
-        self.setPalette(palette)
+        self.setAutoFillBackground(True)  # type: ignore
+        self.setPalette(palette)  # type: ignore
